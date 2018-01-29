fa2db3cfa07d5879e4ebba9fc0609fb2edb1b304
yangqingwen200 <314196705@qq.com>
2018-01-29 14:56:05 +0800

    增加部分注释

--- vuedemo/src/main.js
+++ vuedemo/src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import store from './vuex'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
