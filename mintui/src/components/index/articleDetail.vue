36ca54b528c6a8ac3cef78e9f5c8c3c1e92f248b
yangqingwen200 <314196705@qq.com>
2018-03-08 17:42:21 +0800

    文章详情页面布局

--- mintui/src/components/index/articleDetail.vue
+++ mintui/src/components/index/articleDetail.vue
@@ -1,44 +1,81 @@
 <template>
-  <div style="position: absolute;">
+  <div id="articleDetail">
     <div>
       <div style="font-weight: bold;font-size: 1.25rem">
         中国还有哪鲜为人知可以超越腾讯、阿里的公司仍未上市？
       </div>
-      <div style="text-align: right;margin: 0.5rem">
+      <div style="text-align: right;margin: 0.5rem;font-size: 0.9rem">
         上观 2018-03-07 06:30:47
       </div>
     </div>
     <hr class="my-hr">
-    <div>
-      在全国政协十三届一次会议上，施一公在参与讨论《政府工作报告》时发现，过去5年里，国内有效发明专利拥有量增加了两倍，技术交易额翻了一番。“如果较真一点，假设国内有效发明专利从100增加到300，技术交易额从100变成了200，那么单个专利交易额其实是不增反降了，是5年前的66%。”施一公算了一笔账。<br/>
-      这一数据折射出来的，是科技评价体系的问题。施一公称，在各个单位，不论是晋升还是考量绩效，都会把专利、发表文章、文章的引用数和文章所发表杂志的影响因子作为标准，而且这一风气愈演愈烈。<br/>
-      “但这几个核心的科技评价指标——文章数量、论文引用率、杂志的影响因子——都可以人为地提高。我想大家知道我这句话的意思。”<br/>
-      他表示，各个学校、单位都办了杂志，想要提高影响因子，互相引用就可以，引用多了，‘影响因子’自然也就高了。“中国这样的国家，想要把这3个指标做上去，我认为是易如反掌。”在他看来，论文不足以说明科技实力，美国没有这样的评价方式，科技实力却依然领先。“因此，论文和科技实力是两回事，大家千万要分开。”<br/>
-      不过他也承认，数字指标在各个单位都很重要，影响到评奖、评优、评先：“这是矛盾的两方面，一方面如果没有这些标准，如何反映科研的质量，但有了这些标准，大家拼命‘做’这个数字怎么办？”<br/>
-      “有些文章，通俗点叫‘垃圾文章’，就是纯粹为了发文而发文，这种情况太多了。”谈到业内和社会各界出现的“论文无用”论，施一公表示“坚决反对”。“没有论文，怎么能有科学技术？但在追求论文的导向如此之强的时候，评价立场如此鲜明的时候，造成的负面影响其实非常大。”<br/>
-      如何改变现状？“说实话，我也没有特别好的办法。如果要提建议，就是应该进一步改进科学评价体系，这需要全社会达成一种共识。”施一公介绍，国际通用的方法是参考国际最顶尖专家的观点：“这是一种‘唯专家论’的方式，我不评价这种做法的利弊，但至少在很多国家都是这样的。如果是将本领域所有专家聚在一起，通过抽签确定评价委员会，在科学上，这样的民主是不能带领科学健康向前发展的。”
+    <div id="articleContent">
+      <div>
+        在全国政协十三届一次会议上，施一公在参与讨论《政府工作报告》时发现，过去5年里，国内有效发明专利拥有量增加了两倍，技术交易额翻了一番。“如果较真一点，假设国内有效发明专利从100增加到300，技术交易额从100变成了200，那么单个专利交易额其实是不增反降了，是5年前的66%。”施一公算了一笔账。<br/><br/>
+        这一数据折射出来的，是科技评价体系的问题。施一公称，在各个单位，不论是晋升还是考量绩效，都会把专利、发表文章、文章的引用数和文章所发表杂志的影响因子作为标准，而且这一风气愈演愈烈。<br/><br/>
+        “但这几个核心的科技评价指标——文章数量、论文引用率、杂志的影响因子——都可以人为地提高。我想大家知道我这句话的意思。”<br/><br/>
+        他表示，各个学校、单位都办了杂志，想要提高影响因子，互相引用就可以，引用多了，‘影响因子’自然也就高了。“中国这样的国家，想要把这3个指标做上去，我认为是易如反掌。”在他看来，论文不足以说明科技实力，美国没有这样的评价方式，科技实力却依然领先。“因此，论文和科技实力是两回事，大家千万要分开。”<br/><br/>
+        不过他也承认，数字指标在各个单位都很重要，影响到评奖、评优、评先：“这是矛盾的两方面，一方面如果没有这些标准，如何反映科研的质量，但有了这些标准，大家拼命‘做’这个数字怎么办？”<br/><br/>
+        “有些文章，通俗点叫‘垃圾文章’，就是纯粹为了发文而发文，这种情况太多了。”谈到业内和社会各界出现的“论文无用”论，施一公表示“坚决反对”。“没有论文，怎么能有科学技术？但在追求论文的导向如此之强的时候，评价立场如此鲜明的时候，造成的负面影响其实非常大。”<br/><br/>
+        如何改变现状？“说实话，我也没有特别好的办法。如果要提建议，就是应该进一步改进科学评价体系，这需要全社会达成一种共识。”施一公介绍，国际通用的方法是参考国际最顶尖专家的观点：“这是一种‘唯专家论’的方式，我不评价这种做法的利弊，但至少在很多国家都是这样的。如果是将本领域所有专家聚在一起，通过抽签确定评价委员会，在科学上，这样的民主是不能带领科学健康向前发展的。”
+        在全国政协十三届一次会议上，施一公在参与讨论《政府工作报告》时发现，过去5年里，国内有效发明专利拥有量增加了两倍，技术交易额翻了一番。“如果较真一点，假设国内有效发明专利从100增加到300，技术交易额从100变成了200，那么单个专利交易额其实是不增反降了，是5年前的66%。”施一公算了一笔账。<br/><br/>
+        这一数据折射出来的，是科技评价体系的问题。施一公称，在各个单位，不论是晋升还是考量绩效，都会把专利、发表文章、文章的引用数和文章所发表杂志的影响因子作为标准，而且这一风气愈演愈烈。<br/><br/>
+        “但这几个核心的科技评价指标——文章数量、论文引用率、杂志的影响因子——都可以人为地提高。我想大家知道我这句话的意思。”<br/><br/>
+        他表示，各个学校、单位都办了杂志，想要提高影响因子，互相引用就可以，引用多了，‘影响因子’自然也就高了。“中国这样的国家，想要把这3个指标做上去，我认为是易如反掌。”在他看来，论文不足以说明科技实力，美国没有这样的评价方式，科技实力却依然领先。“因此，论文和科技实力是两回事，大家千万要分开。”<br/><br/>
+        不过他也承认，数字指标在各个单位都很重要，影响到评奖、评优、评先：“这是矛盾的两方面，一方面如果没有这些标准，如何反映科研的质量，但有了这些标准，大家拼命‘做’这个数字怎么办？”<br/><br/>
+        “有些文章，通俗点叫‘垃圾文章’，就是纯粹为了发文而发文，这种情况太多了。”谈到业内和社会各界出现的“论文无用”论，施一公表示“坚决反对”。“没有论文，怎么能有科学技术？但在追求论文的导向如此之强的时候，评价立场如此鲜明的时候，造成的负面影响其实非常大。”<br/><br/>
+        如何改变现状？“说实话，我也没有特别好的办法。如果要提建议，就是应该进一步改进科学评价体系，这需要全社会达成一种共识。”施一公介绍，国际通用的方法是参考国际最顶尖专家的观点：“这是一种‘唯专家论’的方式，我不评价这种做法的利弊，但至少在很多国家都是这样的。如果是将本领域所有专家聚在一起，通过抽签确定评价委员会，在科学上，这样的民主是不能带领科学健康向前发展的。”
+      </div>
+      <div style="text-align: center;margin-top: 1.5rem">
+        <span>
+          <img v-if="!zan" src="../../assets/thumb22.png" align="middle" style="width: 10%" @click="getZan"/>
+          <img v-if="zan" src="../../assets/thumb20.png" align="middle" style="width: 10%" @click="getZan"/>
+          <span style="vertical-align: -webkit-baseline-middle">100</span>
+        </span>
+        <span style="margin-left: 4rem">
+          <img v-if="!cai" src="../../assets/thumb21.png" align="middle" style="width: 10%;" @click="getCai"/>
+          <img v-if="cai" src="../../assets/thumb19.png" align="middle" style="width: 10%;" @click="getCai"/>
+          <span style="vertical-align: -webkit-baseline-middle">200</span>
+        </span>
+      </div>
+
     </div>
     <hr class="my-hr">
-    <div>
+    <div id="articleAd" style="height: 100px">
       广告
     </div>
     <hr class="my-hr">
-    <div>
+    <div id="articleRecommend" style="height: 200px">
       推荐列表
     </div>
     <hr class="my-hr">
-    <div>
-      评论
+    <div id="articleComment" style="margin-bottom: 3.7rem;height: 200px">
+      热门评论
     </div>
     <div class="footer">
-      <span style="padding: 0 0.4rem;font-weight: bold;" @click="goBackList"><</span>
-      <span style="padding-right: 0.2rem"><input style="border-radius: 0.2rem;"/></span>
-      <div style="display: inline-block;">
-        <span>300</span>
-        <span>收藏</span>
-        <span>分享</span>
-      </div>
+      <!-- 不要用v-if, 否则下面的输入框会同时触发focus和blur事件, 没法输入内容 -->
+      <span v-show="!discussing" class="go-back" @click="goBackList">
+        <img src="../../assets/left.png" style="width: 1.5rem"/>
+      </span>
+      <span>
+        <input id="discussing" :class="{'my-input': true, 'my-input-discuss': discussing}" placeholder="发表神评论..."
+               v-model="discuss" v-on:blur="inputBlur" @focus="inputClick"/>
+      </span>
 
+      <span v-if="!discussing" style="text-align: right;padding-right: 1.5rem">
+        <span>
+          <img src="../../assets/message5.png" style="width: 55%" align="middle" @click="goToComments"/>
+          <span class="commentNum">30</span>
+        </span>
+        <span>
+          <img v-show="!collection" src="../../assets/star_empty.png" style="width: 55%" @click="getCollection"/>
+          <img v-show="collection" src="../../assets/star_fill.png" style="width: 55%" @click="getCollection"/>
+
+        </span>
+      </span>
+      <div style="vertical-align: middle;display: table-cell;">
+        <button :class="{'my-button': 'my-button', 'my-button-ok': myButtonOk}" v-if="discussing">发表</button>
+      </div>
     </div>
   </div>
 </template>
@@ -46,9 +83,58 @@
 <script>
   export default {
     name: "article-detail",
+    data() {
+      return {
+        discuss: '',
+        discussing: false,
+        myButtonOk: false,
+        hasClick: false,
+        collection: false,
+        zan: false,
+        cai: false,
+        'my-input': 'my-input',
+        'my-input-discuss': 'my-input-discuss',
+        'my-button': 'my-button',
+        'my-button-ok': 'my-button-ok'
+      }
+    },
     methods: {
       goBackList() {
         window.history.go(-1);
+      },
+      inputBlur() {
+        this.discussing = false;
+      },
+      inputClick() {
+        this.discussing = true;
+      },
+      goToComments() {
+        if (this.hasClick) {
+          window.scrollTo(0, 0);
+        } else {
+          window.scrollTo(0, (document.getElementById("articleDetail").offsetHeight - document.getElementById("articleComment").offsetHeight));
+        }
+        this.hasClick = !this.hasClick;
+      },
+      getCollection() {
+        this.collection = !this.collection;
+      },
+      getZan() {
+        this.zan = !this.zan;
+      },
+      getCai() {
+        this.cai = !this.cai;
+      }
+    },
+    mounted() {
+    },
+    watch: {
+      discuss() {
+        if (this.discuss.trim() === '') {
+          this.myButtonOk = false;
+        } else {
+          this.myButtonOk = true;
+        }
       }
     }
   }
@@ -56,13 +142,70 @@
 
 <style scoped>
   .footer {
-    height: 1.62rem;
+    height: 2.5rem;
     position: fixed;
     bottom: 0;
-    background-color: red;
     width: 96%;
+    display: table;
+    background-color: #fdfdfd;
+    padding: 0.2rem 0;
+    border: 1px solid #ebebeb;
+    border-bottom: none;
+    border-left: none;
+    border-right: none;
+
   }
+
+  .go-back {
+    padding: 0 1.5rem 0 0.5rem;
+    font-size: 2.0rem
+  }
+
   div.footer span {
-    margin-left: 0.2rem;
+    vertical-align: middle;
+    display: table-cell;
+  }
+
+  .my-input {
+    border-radius: 0.25rem;
+    height: 2.0rem;
+    font-size: 0.9rem;
+    width: 150px;
+    border: 1px solid #a9acb1;
+    padding-left: 0.5rem;
+  }
+
+  .my-input-discuss {
+    width: 95%;
+  }
+
+  .my-button {
+    height: 2.0rem;
+    border-radius: 0.25rem;
+    border: 0px solid #a9acb1;
+    padding: 0 0.6rem
+  }
+
+  .my-button-ok {
+    background-color: #26a2ff;
+    color: aliceblue;
+  }
+
+  .btn {
+    padding: 0.5rem 4rem;
+    border-radius: 1rem;
+    border: 1px solid #d9d9d9;
+    background-color: #fdfdfd;
+  }
+
+  .commentNum {
+    float: right;
+    margin-left: -15px;
+    background-color: red;
+    color: #fafafa;
+    border-radius: 0.5rem;
+    padding: 0.05rem 0.2rem;
+    font-size: 0.7rem;
+    position: relative;
   }
 </style>
