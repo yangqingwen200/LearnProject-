24330cd0f95ccd44773f375ada804e56e4b0d672
yangqingwen200 <314196705@qq.com>
2018-03-09 19:55:17 +0800

    文章详情页面布局_微调

--- mintui/src/components/index/articleDetail.vue
+++ mintui/src/components/index/articleDetail.vue
@@ -18,63 +18,84 @@
         不过他也承认，数字指标在各个单位都很重要，影响到评奖、评优、评先：“这是矛盾的两方面，一方面如果没有这些标准，如何反映科研的质量，但有了这些标准，大家拼命‘做’这个数字怎么办？”<br/><br/>
         “有些文章，通俗点叫‘垃圾文章’，就是纯粹为了发文而发文，这种情况太多了。”谈到业内和社会各界出现的“论文无用”论，施一公表示“坚决反对”。“没有论文，怎么能有科学技术？但在追求论文的导向如此之强的时候，评价立场如此鲜明的时候，造成的负面影响其实非常大。”<br/><br/>
         如何改变现状？“说实话，我也没有特别好的办法。如果要提建议，就是应该进一步改进科学评价体系，这需要全社会达成一种共识。”施一公介绍，国际通用的方法是参考国际最顶尖专家的观点：“这是一种‘唯专家论’的方式，我不评价这种做法的利弊，但至少在很多国家都是这样的。如果是将本领域所有专家聚在一起，通过抽签确定评价委员会，在科学上，这样的民主是不能带领科学健康向前发展的。”
-        在全国政协十三届一次会议上，施一公在参与讨论《政府工作报告》时发现，过去5年里，国内有效发明专利拥有量增加了两倍，技术交易额翻了一番。“如果较真一点，假设国内有效发明专利从100增加到300，技术交易额从100变成了200，那么单个专利交易额其实是不增反降了，是5年前的66%。”施一公算了一笔账。<br/><br/>
-        这一数据折射出来的，是科技评价体系的问题。施一公称，在各个单位，不论是晋升还是考量绩效，都会把专利、发表文章、文章的引用数和文章所发表杂志的影响因子作为标准，而且这一风气愈演愈烈。<br/><br/>
-        “但这几个核心的科技评价指标——文章数量、论文引用率、杂志的影响因子——都可以人为地提高。我想大家知道我这句话的意思。”<br/><br/>
-        他表示，各个学校、单位都办了杂志，想要提高影响因子，互相引用就可以，引用多了，‘影响因子’自然也就高了。“中国这样的国家，想要把这3个指标做上去，我认为是易如反掌。”在他看来，论文不足以说明科技实力，美国没有这样的评价方式，科技实力却依然领先。“因此，论文和科技实力是两回事，大家千万要分开。”<br/><br/>
-        不过他也承认，数字指标在各个单位都很重要，影响到评奖、评优、评先：“这是矛盾的两方面，一方面如果没有这些标准，如何反映科研的质量，但有了这些标准，大家拼命‘做’这个数字怎么办？”<br/><br/>
-        “有些文章，通俗点叫‘垃圾文章’，就是纯粹为了发文而发文，这种情况太多了。”谈到业内和社会各界出现的“论文无用”论，施一公表示“坚决反对”。“没有论文，怎么能有科学技术？但在追求论文的导向如此之强的时候，评价立场如此鲜明的时候，造成的负面影响其实非常大。”<br/><br/>
-        如何改变现状？“说实话，我也没有特别好的办法。如果要提建议，就是应该进一步改进科学评价体系，这需要全社会达成一种共识。”施一公介绍，国际通用的方法是参考国际最顶尖专家的观点：“这是一种‘唯专家论’的方式，我不评价这种做法的利弊，但至少在很多国家都是这样的。如果是将本领域所有专家聚在一起，通过抽签确定评价委员会，在科学上，这样的民主是不能带领科学健康向前发展的。”
       </div>
-      <div style="text-align: center;margin-top: 1.5rem">
-        <span @click="getZan">
-          <img v-if="!zan" src="../../assets/thumb22.png" align="middle" style="width: 10%" />
-          <img v-if="zan" src="../../assets/thumb20.png" align="middle" style="width: 10%"/>
-          <span style="vertical-align: -webkit-baseline-middle;margin-left: -5px">999</span>
+      <div style="text-align: center;margin-top: 1.5rem;margin-bottom: 1rem">
+        <span @click="getZanContent" class="zan-cai-num">
+          <img v-if="!zanContent" src="../../assets/thumb22.png" align="middle" style="width: 10%"/>
+          <img v-if="zanContent" src="../../assets/thumb20.png" align="middle" style="width: 10%"/>
+          <span style="vertical-align: -webkit-baseline-middle;margin-left: -5px">888</span>
         </span>
-        <span @click="getCai" style="margin-left: 15%">
-          <img v-if="!cai" src="../../assets/thumb21.png" align="middle" style="width: 10%;" />
-          <img v-if="cai" src="../../assets/thumb19.png" align="middle" style="width: 10%;"/>
+        <span @click="getCaiContent" class="zan-cai-num" style="margin-left: 10%">
+          <img v-if="!caiContent" src="../../assets/thumb21.png" align="middle" style="width: 10%;"/>
+          <img v-if="caiContent" src="../../assets/thumb19.png" align="middle" style="width: 10%;"/>
           <span style="vertical-align: -webkit-baseline-middle;margin-left: -5px">9999</span>
         </span>
       </div>
 
     </div>
     <hr class="my-hr">
-    <div id="articleAd" style="height: 100px">
-      广告
-    </div>
-    <hr class="my-hr">
-    <div id="articleRecommend" style="height: 200px">
-      推荐列表
-    </div>
-    <hr class="my-hr">
-    <div id="articleComment" style="margin-bottom: 3.7rem;height: 200px">
-      热门评论
+    <div id="articleComment" style="margin-bottom: 3rem;">
+      <div>
+        <span v-for="item in comments">
+          <table class="comment-table" border="0" cellpadding="0" cellspacing="0">
+            <tr>
+              <td rowspan="4" style="width: 15%;" align="center" valign="top">
+                <img src="../../assets/header.jpeg" style="width: 80%;border-radius: 50%;margin-top: 15%"/>
+              </td>
+            </tr>
+            <tr>
+              <td style="color: #26a2ff">游客_1573</td>
+              <td align="right">
+                <img :src="zanCommentPicUrl" style="width: 0.8rem;vertical-align: middle" @click="addZan($event)"/>
+                <span style="vertical-align: middle;">123</span>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span v-html="item.content"></span>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <span v-html="item.time"></span> ·
+                <div class="comment-res">
+                  123回复
+                </div>
+              </td>
+            </tr>
+          </table>
+        </span>
+      </div>
     </div>
     <div class="footer">
-      <!-- 不要用v-if, 否则下面的输入框会同时触发focus和blur事件, 没法输入内容 -->
-      <span v-show="!discussing" class="go-back" @click="goBackList">
-        <img src="../../assets/arrow270.png" align="middle" style="width: 1.3rem"/>
-      </span>
-      <span>
-        <input id="discussing" :class="{'my-input': true, 'my-input-discuss': discussing}" placeholder="发表神评论..."
-               v-model="discuss" v-on:blur="inputBlur" @focus="inputClick"/>
-      </span>
+      <table cellspacing="0" cellpadding="0" style="width: 100%">
+        <tr>
+          <td v-show="!discussing" @click="goBackList" style="padding: 0 1rem 0.3rem 0.7rem;">
+            <img src="../../assets/arrow270.png" align="middle" style="width: 1.3rem"/>
+          </td>
+          <td>
+            <input id="discussing" :class="{'my-input': true, 'my-input-discuss': discussing}" :placeholder="placeholder"
+                   v-model="discuss" v-on:blur="inputBlur" @focus="inputClick"/>
+          </td>
+          <td v-if="!discussing" @click="goToComments">
+            <img src="../../assets/message5.png" style="width: 1.8rem;float: left" align="middle"/>
+            <span class="commentNum">99+</span>
+          </td>
+          <td v-if="!discussing" @click="getCollection">
+            <img v-show="!collection" src="../../assets/star_empty.png" align="middle"
+                 style="width: 1.8rem;margin: 0 0.5rem;"/>
+            <img v-show="collection" src="../../assets/star_fill.png" align="middle"
+                 style="width: 1.8rem;margin: 0 0.5rem;"/>
+          </td>
+          <td v-if="!discussing" @click="report">
+            <img src="../../assets/light53.png" align="middle" style="width: 1.65rem;margin: 0 0.5rem;"/>
+          </td>
+          <td v-if="discussing" @click="pubComment" align="middle">
+            <button :class="{'my-button': 'my-button', 'my-button-ok': myButtonOk, 'my-button-bao': this.discuss.trim() !== '' && isJuBao}">{{buttonValue}}</button>
+          </td>
+        </tr>
 
-      <span v-if="!discussing" style="text-align: right;padding-right: 1.5rem">
-        <span @click="goToComments">
-          <img src="../../assets/message5.png" style="width: 1.8rem" align="middle"/>
-          <span class="commentNum">3.1万</span>
-        </span>
-        <span style="padding-left: 1.0rem" @click="getCollection">
-          <img v-show="!collection" src="../../assets/star_empty.png" align="middle" style="width: 1.8rem" />
-          <img v-show="collection" src="../../assets/star_fill.png" align="middle" style="width: 1.8rem"/>
-        </span>
-      </span>
-      <div style="vertical-align: middle;display: table-cell;">
-        <button :class="{'my-button': 'my-button', 'my-button-ok': myButtonOk}" v-if="discussing">发表</button>
-      </div>
+      </table>
     </div>
   </div>
 </template>
@@ -84,18 +105,28 @@
     name: "article-detail",
     data() {
       return {
+        buttonValue: '',
+        placeholder: '发表神评论...',
+        isJuBao: false,
         discuss: '',
         discussing: false,
         myButtonOk: false,
         hasClick: false,
         collection: false,
-        zan: false,
-        cai: false,
+        zanContent: false,
+        caiContent: false,
+        zanCommentPicUrl: require('../../assets/icon_like.png'), //动态切换图片
         'my-input': 'my-input',
         'my-input-discuss': 'my-input-discuss',
         'my-button': 'my-button',
-        'my-button-ok': 'my-button-ok'
-      }
+        'my-button-ok': 'my-button-ok',
+        'my-button-bao': 'my-button-bao',
+        comments: [
+          {content: "JavaScript默认的时间格式我们一般情况下不会用", time: new Date().toLocaleString()},
+          {content: "我发现评论都有好多版本啊。我亲身经历过，讲下我经历的版本。就是原先北方大区有个玩家玩CF猝死了，而后北方大区就出现了所谓的闹鬼（其实后来是证实了黑客在搞鬼）时间是每晚的11.30分", time: new Date().toLocaleString()},
+          {content: "一个飞虎队，一个猎狐者，还有个母体僵尸。然后这三个排队绕着固定路线跑来跑去，头上都是没名字的。打也打不到，抓也抓不到。除此之外我们还能正常玩游戏，等到了第8个回合结束，本以为就这么结束退游戏，但是又循环到了第2回合", time: new Date().toLocaleString()},
+        ]
+      };
     },
     methods: {
       goBackList() {
@@ -103,9 +134,20 @@
       },
       inputBlur() {
         this.discussing = false;
+        if(this.discuss.trim() === '' && this.isJuBao) {
+          this.placeholder = '发表神评论...';
+          this.isJuBao = false;
+        }
       },
       inputClick() {
         this.discussing = true;
+        if(this.isJuBao) {
+          this.buttonValue = '举报';
+          this.placeholder = '请填写举报理由...';
+        } else {
+          this.buttonValue = '发布';
+          this.placeholder = '发表神评论...';
+        }
       },
       goToComments() {
         if (this.hasClick) {
@@ -118,11 +160,35 @@
       getCollection() {
         this.collection = !this.collection;
       },
-      getZan() {
-        this.zan = !this.zan;
+      getZanContent() {
+        this.zanContent = !this.zanContent;
       },
-      getCai() {
-        this.cai = !this.cai;
+      getCaiContent() {
+        this.caiContent = !this.caiContent;
+      },
+      pubComment() {
+        if(this.discuss.trim() === '') {
+          this.isJuBao = false;
+          this.placeholder = '发表神评论...';
+          return;
+        }
+        if(this.isJuBao) {
+          console.info("举报成功: " + this.discuss);
+          this.isJuBao = false;
+          this.placeholder = '发表神评论...';
+        } else {
+          this.comments.unshift({content: this.discuss, time: new Date().toLocaleString()});
+          window.scrollTo(0, (document.getElementById("articleDetail").offsetHeight - document.getElementById("articleComment").offsetHeight) - 10);
+        }
+        this.discuss = '';
+      },
+      addZan(e) {
+        e.target.src = require('../../assets/icon_like_alt.png');
+        e.target.nextElementSibling.innerHTML = "456"
+      },
+      report() {
+        this.isJuBao = true;
+        document.getElementById('discussing').focus();
       }
     },
     mounted() {
@@ -140,6 +206,14 @@
 </script>
 
 <style scoped>
+  .comment-table {
+    width: 100%;
+    font-size: 0.65rem;
+    border-collapse: separate;
+    border-spacing: 0px 0.4rem;
+    color: black;
+  }
+
   .footer {
     height: 2.0rem;
     position: fixed;
@@ -147,16 +221,12 @@
     width: 96%;
     display: table;
     background-color: #fdfdfd;
-    padding: 0.2rem 0;
+    padding: 0.3rem 0;
     border: 1px solid #ebebeb;
     border-bottom: none;
     border-left: none;
     border-right: none;
 
-  }
-
-  .go-back {
-    padding: 0 0.5rem 0.2rem 0.5rem;
   }
 
   div.footer span {
@@ -165,23 +235,24 @@
   }
 
   .my-input {
-    border-radius: 0.25rem;
-    height: 1.5rem;
+    border-radius: 0.2rem;
+    height: 1.7rem;
     font-size: 0.9rem;
-    width: 150px;
     border: 1px solid #a9acb1;
     padding-left: 0.5rem;
+    width: 80%;
   }
 
   .my-input-discuss {
-    width: 95%;
+    width: 100%;
   }
 
   .my-button {
-    height: 2.0rem;
+    height: 1.9rem;
     border-radius: 0.25rem;
     border: 0px solid #a9acb1;
-    padding: 0 0.6rem
+    margin-left: 0.5rem;
+    padding: 0 1rem;
   }
 
   .my-button-ok {
@@ -189,21 +260,31 @@
     color: aliceblue;
   }
 
-  .btn {
-    padding: 0.5rem 4rem;
-    border-radius: 1rem;
-    border: 1px solid #d9d9d9;
-    background-color: #fdfdfd;
-  }
-
   .commentNum {
     float: right;
-    margin-left: -12px;
+    margin-left: -1.25rem;
     background-color: #26a2ff;
     color: #fafafa;
     border-radius: 0.4rem;
     padding: 0 0.25rem;
     font-size: 0.7rem;
-    position: relative;
+  }
+
+  .comment-res {
+    display: inline-block;
+    background-color: #f0f0f0;
+    border-radius: 1rem;
+    padding: 0.2rem 0.3rem;
+  }
+
+  .zan-cai-num {
+    border: 1px solid #ebebeb;
+    height: 16rem;
+    padding: 0.5rem 2rem 1rem;
+    border-radius: 2rem;
+  }
+
+  .my-button-bao {
+    background-color: red;
   }
 </style>
